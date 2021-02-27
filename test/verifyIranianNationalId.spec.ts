21526ecf99685bb266181777b97a251765355388
torki <a.torki@hasintech.com>
2021-02-28 01:45:06 +0330

    test(verifyIranianNationalId): add a more test

--- test/verifyIranianNationalId.spec.ts
+++ test/verifyIranianNationalId.spec.ts
@@ -7,6 +7,8 @@ it("Validation of Iranian National Number(code-e Melli)", () => {
 	expect(verifyIranianNationalId()).toBeUndefined();
 
 	expect(verifyIranianNationalId("0499370899")).not.toBeFalsy();
+	// https://www.yjc.ir/fa/news/6787095/%D8%B1%D9%86%D8%AF%D8%AA%D8%B1%DB%8C%D9%86-%D8%B4%D9%85%D8%A7%D8%B1%D9%87-%D9%85%D9%84%DB%8C-%DA%A9%D8%B4%D9%88%D8%B1-%D8%B1%D8%A7-%D8%A8%D8%B4%D9%86%D8%A7%D8%B3%DB%8C%D8%AF
+	expect(verifyIranianNationalId("1111111111")).not.toBeFalsy();
 	expect(verifyIranianNationalId("0790419904")).not.toBeFalsy();
 	expect(verifyIranianNationalId("0084575948")).not.toBeFalsy();
 	expect(verifyIranianNationalId("0963695398")).not.toBeFalsy();
