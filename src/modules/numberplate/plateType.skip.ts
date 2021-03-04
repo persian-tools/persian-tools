c13851168a159ae1da0f4dd36d0ae4658a1b9438
mediv0 <anonymousaxis@gmail.com>
2021-03-04 10:01:01 +0330

    Create plate character category Object

---
+++ src/modules/numberplate/plateType.skip.ts
@@ -0,0 +1,106 @@
+export default [
+	{
+		label: "D",
+		description: "دیپلمات",
+	},
+	{
+		label: "S",
+		description: "سفارتخانه ها ",
+	},
+	{
+		label: "ف",
+		description: "ستاد کل نیرو های مسلح",
+	},
+	{
+		label: "ز",
+		description: "وزارت دفاع و پشتیبانی",
+	},
+	{
+		label: "ش",
+		description: "ارتش",
+	},
+	{
+		label: "تشریفات",
+		description: "تشریفات",
+	},
+	{
+		label: "الف",
+		description: "دولتی",
+	},
+	{
+		label: "پ",
+		description: "پلیس",
+	},
+	{
+		label: "ث",
+		description: "سپاه",
+	},
+	{
+		label: "ک",
+		description: "کشاورزی",
+	},
+	{
+		label: "ع",
+		description: "حمل و نقل عمومی",
+	},
+	{
+		label: "گ",
+		description: "گذر موقت",
+	},
+	{
+		label: "ژ",
+		description: "معلولان و جانبازان",
+	},
+	{
+		label: "ب",
+		description: "شخصی",
+	},
+	{
+		label: "ج",
+		description: "شخصی",
+	},
+	{
+		label: "د",
+		description: "شخصی",
+	},
+	{
+		label: "س",
+		description: "شخصی",
+	},
+	{
+		label: "ص",
+		description: "شخصی",
+	},
+	{
+		label: "ط",
+		description: "شخصی",
+	},
+	{
+		label: "ق",
+		description: "شخصی",
+	},
+	{
+		label: "ل",
+		description: "شخصی",
+	},
+	{
+		label: "م",
+		description: "شخصی",
+	},
+	{
+		label: "ن",
+		description: "شخصی",
+	},
+	{
+		label: "و",
+		description: "شخصی",
+	},
+	{
+		label: "ه",
+		description: "شخصی",
+	},
+	{
+		label: "ی",
+		description: "شخصی",
+	},
+];
