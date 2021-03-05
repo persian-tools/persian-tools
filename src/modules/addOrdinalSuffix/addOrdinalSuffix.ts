a8636e6cdbc0083af8d7db9ed02bbbe53aa7bd37
Masoud <msdDaliriyan@gmai.com>
2021-03-06 01:41:18 +0330

    fix:clean up addOrdinaSuffix and add error handling

--- src/modules/addOrdinalSuffix/addOrdinalSuffix.ts
+++ src/modules/addOrdinalSuffix/addOrdinalSuffix.ts
@@ -4,16 +4,20 @@
  * @param number - Number, eg: "300000"
  * @return A string of ordinated number
  */
-const addOrdinalSuffix = (number?: string): string | undefined => {
-	if (typeof number === "undefined") return;
+const addOrdinalSuffix = (number?: string): string => {
+    if (typeof number !== 'string') {
+        throw new TypeError("input must be string")
+    }
 
-	if (number.endsWith("ی")) {
-		number += " اُم";
-	} else if (number.endsWith("سه")) {
-		number = number.slice(0, -2) + "سوم";
-	} else number += "م";
+    if (number.endsWith("ی")) {
+        return number + " اُم"
+    }
 
-	return number;
+    if (number.endsWith("سه")) {
+        return number.slice(0, -2) + "سوم";
+    }
+
+    return number + "م"
 };
 
 export default addOrdinalSuffix;
