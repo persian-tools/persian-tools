434a40804c9375be5403ac0821d37e138c4c9fe1
mediv0 <anonymousaxis@gmail.com>
2021-03-09 17:30:10 +0330

    refactor: remove runtime hashtable creator

--- src/modules/numberplate/helpers.ts
+++ src/modules/numberplate/helpers.ts
@@ -30,18 +30,3 @@ export const isPlateNumberValid = (numbers: string): boolean => {
 		.slice(0, -1)
 		.every((num) => +num !== 0);
 };
-
-export const createProvinceHashTable = (provinceDataset: ProvinceDataset): { [index: string]: string } => {
-	const hashTable: {
-		[index: string]: string;
-	} = {};
-	for (let i = 0; i < provinceDataset.length; i++) {
-		const _province = provinceDataset[i];
-		for (let j = 0; j < _province.codes.length; j++) {
-			const _code = _province.codes[j];
-			hashTable[_code] = _province.province;
-		}
-	}
-
-	return hashTable;
-};
