e8f3b4ebe9781eb08577bd137adbaf75aea3a112
Masoud <msdDaliriyan@gmai.com>
2021-03-09 12:14:20 +0330

    fix(addOrdinalSuffix): add test

--- src/modules/addOrdinalSuffix/addOrdinalSuffix.ts
+++ src/modules/addOrdinalSuffix/addOrdinalSuffix.ts
@@ -5,19 +5,19 @@
  * @return A string of ordinated number
  */
 const addOrdinalSuffix = (number?: string): string => {
-    if (typeof number !== 'string') {
-        throw new TypeError("input must be string")
-    }
+	if (typeof number !== "string") {
+		throw new TypeError("input must be string");
+	}
 
-    if (number.endsWith("ی")) {
-        return number + " اُم"
-    }
+	if (number.endsWith("ی")) {
+		return number + " اُم";
+	}
 
-    if (number.endsWith("سه")) {
-        return number.slice(0, -2) + "سوم";
-    }
+	if (number.endsWith("سه")) {
+		return number.slice(0, -2) + "سوم";
+	}
 
-    return number + "م"
+	return number + "م";
 };
 
 export default addOrdinalSuffix;
