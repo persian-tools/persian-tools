87a035d377cf8249c6f58b7d3bf977586f12a4bc
mediv0 <anonymousaxis@gmail.com>
2021-03-17 20:05:21 +0330

    test(addCommas): add tests for floating-point numbers

--- test/addCommas.spec.ts
+++ test/addCommas.spec.ts
@@ -6,6 +6,11 @@ it("Add and remove commas", () => {
 	expect(addCommas("30,000,000")).toEqual("30,000,000");
 	expect(addCommas("۳۰۰۰۰۰۰۰")).toEqual("30,000,000");
 
+	// float numbers
+	expect(addCommas("30,000,000.02")).toEqual("30,000,000.02");
+	expect(addCommas("12500.9")).toEqual("12,500.9");
+	expect(addCommas(12500.9)).toEqual("12,500.9");
+
 	expect(addCommas(300)).toEqual("300");
 	// @ts-ignore
 	expect(addCommas(3000)).toBeType("string");
