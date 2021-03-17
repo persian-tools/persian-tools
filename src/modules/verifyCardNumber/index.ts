2ceb4aa0682f726efc8040906ac65f29e3081ca1
= <=>
2021-03-17 19:05:58 +0330

    use prettier

--- src/modules/verifyCardNumber/index.ts
+++ src/modules/verifyCardNumber/index.ts
@@ -7,26 +7,28 @@
  * @return {Boolean}
  */
 function verifyCardNumber(digits: number): boolean | undefined {
-    if (!digits) return;
-    const digitsResult = String(digits);
+	if (!digits) return;
+	const digitsResult = String(digits);
 
-    const length = digitsResult.length;
-    if (
-        length < 16 ||
-        parseInt(digitsResult.substr(1, 10), 10) === 0 ||
-        parseInt(digitsResult.substr(10, 6), 10) === 0
-    ) {
-        return false;
-    }
+	const length = digitsResult.length;
+	if (
+		length < 16 ||
+		parseInt(digitsResult.substr(1, 10), 10) === 0 ||
+		parseInt(digitsResult.substr(10, 6), 10) === 0
+	) {
+		return false;
+	}
 
-    let oddOrEven, subDigit, sum = 0;
+	let oddOrEven,
+		subDigit,
+		sum = 0;
 
-    for (let i = 0; i < 16; i++) {
-        oddOrEven = (i % 2 === 0) ? 2 : 1;
-        subDigit = parseInt(digitsResult.substr(i, 1), 10) * oddOrEven;
-        sum += subDigit > 9 ? subDigit - 9 : subDigit;
-    }
-    return sum % 10 === 0;
+	for (let i = 0; i < 16; i++) {
+		oddOrEven = i % 2 === 0 ? 2 : 1;
+		subDigit = parseInt(digitsResult.substr(i, 1), 10) * oddOrEven;
+		sum += subDigit > 9 ? subDigit - 9 : subDigit;
+	}
+	return sum % 10 === 0;
 }
 
 export default verifyCardNumber;
