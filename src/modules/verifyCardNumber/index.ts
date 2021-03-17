5f3f09f9223adff9410600f75a707f830447c868
= <=>
2021-03-17 18:27:28 +0330

    some clean code and add more test case

--- src/modules/verifyCardNumber/index.ts
+++ src/modules/verifyCardNumber/index.ts
@@ -1,33 +1,32 @@
 /**
- * Verify Iranian Bank's card number which is valid or not
+ * @description Verify Iranian Bank's card number which is valid or not
  *
  * @public
  * @method verifyCardNumber
- * @param digits
- * @return boolean
+ * @param {Number} digits - card number
+ * @return {Boolean}
  */
 function verifyCardNumber(digits: number): boolean | undefined {
-	if (!digits) return;
+    if (!digits) return;
+    const digitsResult = String(digits);
 
-	const digitsResult = "" + digits;
+    const length = digitsResult.length;
+    if (
+        length < 16 ||
+        parseInt(digitsResult.substr(1, 10), 10) === 0 ||
+        parseInt(digitsResult.substr(10, 6), 10) === 0
+    ) {
+        return false;
+    }
 
-	const length = digitsResult.length;
-	if (
-		length < 16 ||
-		parseInt(digitsResult.substr(1, 10), 10) === 0 ||
-		parseInt(digitsResult.substr(10, 6), 10) === 0
-	) {
-		return false;
-	}
+    let oddOrEven, subDigit, sum = 0;
 
-	let sum = 0;
-	let even, subDigit;
-	for (let i = 0; i < 16; i++) {
-		even = i % 2 === 0 ? 2 : 1;
-		subDigit = parseInt(digitsResult.substr(i, 1), 10) * even;
-		sum += subDigit > 9 ? subDigit - 9 : subDigit;
-	}
-	return sum % 10 === 0;
+    for (let i = 0; i < 16; i++) {
+        oddOrEven = (i % 2 === 0) ? 2 : 1;
+        subDigit = parseInt(digitsResult.substr(i, 1), 10) * oddOrEven;
+        sum += subDigit > 9 ? subDigit - 9 : subDigit;
+    }
+    return sum % 10 === 0;
 }
 
 export default verifyCardNumber;
--- test/verifyCardNumber.spec.ts
+++ test/verifyCardNumber.spec.ts
@@ -1,9 +1,11 @@
 import { verifyCardNumber } from "../src";
 
 it("Bank number validation", () => {
-	expect(verifyCardNumber(6037701689095443)).not.toBeFalsy();
-	expect(verifyCardNumber(6219861034529007)).not.toBeFalsy();
+	expect(verifyCardNumber(6037701689095443)).toBeTruthy();
+	expect(verifyCardNumber(6219861034529007)).toBeTruthy();
+
 	expect(verifyCardNumber(6219861034529008)).toBeFalsy();
 	expect(verifyCardNumber(621986103452900)).toBeFalsy();
+
 	expect(verifyCardNumber(0)).toBeFalsy();
 });
