91c0b15c46705dd135f695a363dee0825c181041
= <=>
2021-03-30 15:06:55 +0430

    fix todo. use localeCompare for sorting

--- src/modules/sortText/index.ts
+++ src/modules/sortText/index.ts
@@ -1,43 +1,15 @@
-/** SortText
- *
- *  Description: Takes a piece of text in Persian which contains
- *  several lines (separated by the newline character), and sorts
- *  the lines alphabetically, with respect to their first character.
- *
- */
-function SortText(str: string): string | string[] | undefined {
+/** SortText * *
+ * @description Takes a piece of text in Persian which contains * several lines (separated by the newline character), and sorts
+ * * the lines alphabetically, with respect to their first character.
+ * * @param {string} str
+ * * @return { string[] | undefined}
+ * */
+
+function SortText(str: string | string[]): string[] | undefined {
 	if (!str) return;
-
-	const sortPreparation = (instr: string): string => {
-		// solve persian problem on sorting by replace characters in strings
-		return instr
-			.replace(/ی/g, "ي")
-			.replace(/ک/g, "ك")
-			.replace(/ھ/g, "ه")
-			.replace(/پ/g, "بی")
-			.replace(/چ/g, "جی")
-			.replace(/ڕ/g, "ری")
-			.replace(/ژ/g, "زی")
-			.replace(/ڤ/g, "فی")
-			.replace(/ڵ/g, "لی")
-			.replace(/گ/g, "كی")
-			.replace(/ۆ/g, "وی")
-			.replace(/ە/g, "هی")
-			.replace(/ێ/g, "يي");
-	};
-
-	const temp = str.split(" ");
-	temp.sort((a, b) => {
-		const keyA = sortPreparation(a);
-		const keyB = sortPreparation(b);
-
-		if (keyA < keyB) return -1;
-		if (keyA > keyB) return 1;
-
-		return 0;
-	});
-
-	return temp;
+	const stringResult = typeof str === "string" ? str.split(" ") : str;
+	stringResult.sort((a, b) => a.localeCompare(b, "fa", { ignorePunctuation: true }));
+	return stringResult;
 }
 
 export default SortText;
