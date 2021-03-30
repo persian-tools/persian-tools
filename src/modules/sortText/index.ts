f03a777c74244f8122f749c134a24f553cdc4a02
= <=>
2021-03-30 15:07:47 +0430

    minor changes

--- src/modules/sortText/index.ts
+++ src/modules/sortText/index.ts
@@ -1,8 +1,9 @@
 /** SortText * *
- * @description Takes a piece of text in Persian which contains * several lines (separated by the newline character), and sorts
- * * the lines alphabetically, with respect to their first character.
- * * @param {string} str
- * * @return { string[] | undefined}
+ * @description Takes a piece of text in Persian which contains
+ * several lines (separated by the newline character), and sorts
+ * the lines alphabetically, with respect to their first character.
+ * @param {string} str
+ * @return { string[] | undefined}
  * */
 
 function SortText(str: string | string[]): string[] | undefined {
