c83b82f27e21a669e050bb43ffbb2002824d8ded
= <=>
2021-04-01 12:11:39 +0430

    Test: add more test case

--- test/URLfix.spec.ts
+++ test/URLfix.spec.ts
@@ -1,14 +1,18 @@
-import { URLfix } from "../src";
+import {URLfix} from "../src";
 
 it("URLfix", () => {
-	expect(
-		URLfix(
-			"https://fa.wikipedia.org/wiki/%D9%85%D8%AF%DB%8C%D8%A7%D9%88%DB%8C%DA%A9%DB%8C:Gadget-Extra-Editbuttons-botworks.js",
-		),
-	).toEqual("https://fa.wikipedia.org/wiki/مدیاویکی:Gadget-Extra-Editbuttons-botworks.js");
-	expect(URLfix("https://en.wikipedia.org/wiki/Persian_alphabet")).toEqual(
-		"https://en.wikipedia.org/wiki/Persian_alphabet",
-	);
-	expect(URLfix()).toBeUndefined();
-	expect(URLfix("Sample Text")).toEqual("Sample Text");
+    expect(
+        URLfix(
+            "https://fa.wikipedia.org/wiki/%D9%85%D8%AF%DB%8C%D8%A7%D9%88%DB%8C%DA%A9%DB%8C:Gadget-Extra-Editbuttons-botworks.js",
+        ),
+    ).toEqual("https://fa.wikipedia.org/wiki/مدیاویکی:Gadget-Extra-Editbuttons-botworks.js");
+
+    expect(URLfix("https://en.wikipedia.org/wiki/Persian_alphabet")).toEqual(
+        "https://en.wikipedia.org/wiki/Persian_alphabet",
+    );
+
+    expect(URLfix('https://fa.wikipedia.org/wiki/%D8%AF%DB%8C%D8%A7%DA%A9%D9%88')).toEqual('https://fa.wikipedia.org/wiki/دیاکو')
+    expect(URLfix('https://fa.wikipedia.org/wiki/%D9%85%DA%A9%D8%A7%D9%86%DB%8C%DA%A9%20%DA%A9%D9%88%D8%A7%D9%86%D8%AA%D9%88%D9%85%DB%8C')).toEqual('https://fa.wikipedia.org/wiki/مکانیک کوانتومی')
+    expect(URLfix("Sample Text")).toEqual("Sample Text");
+    expect(URLfix()).toBeUndefined();
 });
