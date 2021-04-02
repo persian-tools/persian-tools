cbe0e9fafadbdeb7e893347d0d5fee85216ef322
Mahdi Momeni <mahdimomeni6@gmail.com>
2021-04-02 11:34:42 +0430

    add test case for hypen and some minor changes

--- src/modules/URLfix/index.ts
+++ src/modules/URLfix/index.ts
@@ -2,21 +2,20 @@
  * @method URLfix
  * @description Used for fix Persian characters in URL
  * @param {string} url
- * @param {string} separator - optional argument to replace Character with space in URLs. by default return with space
+ * @param {string} separator - optional argument to replace character with space in URLs. by default return URL with space
  * @example
  * URLfix('https://fa.wikipedia.org/wiki/%D9%85%DA%A9%D8%A7%D9%86%DB%8C%DA%A9%20%DA%A9%D9%88%D8%A7%D9%86%D8%AA%D9%88%D9%85%DB%8C', '_')
  * return 'https://fa.wikipedia.org/wiki/مکانیک_کوانتومی'
- * @return a string of fixed URL
+ * @return {string} a string of fixed URL
  */
+
 function URLfix(url?: string, separator?: string): string | undefined {
 	if (!url) return;
 	url = decodeURIComponent(url);
 
-	if (separator) {
-		url = url.replace(" ", separator);
-	}
+	if (separator) return url.replace(" ", separator);
 
 	return url;
 }
 
-export default URLfix;
+export default URLfix
--- test/URLfix.spec.ts
+++ test/URLfix.spec.ts
@@ -5,6 +5,7 @@ it("URLfix", () => {
     expect(URLfix("https://en.wikipedia.org/wiki/Persian_alphabet")).toEqual("https://en.wikipedia.org/wiki/Persian_alphabet",);
     expect(URLfix('https://fa.wikipedia.org/wiki/%D8%AF%DB%8C%D8%A7%DA%A9%D9%88')).toEqual('https://fa.wikipedia.org/wiki/دیاکو')
     expect(URLfix('https://fa.wikipedia.org/wiki/%D9%85%DA%A9%D8%A7%D9%86%DB%8C%DA%A9%20%DA%A9%D9%88%D8%A7%D9%86%D8%AA%D9%88%D9%85%DB%8C', '_')).toEqual('https://fa.wikipedia.org/wiki/مکانیک_کوانتومی')
+    expect(URLfix('https://fa.wikipedia.org/wiki/%D9%85%DA%A9%D8%A7%D9%86%DB%8C%DA%A9%20%DA%A9%D9%88%D8%A7%D9%86%D8%AA%D9%88%D9%85%DB%8C', '-')).toEqual('https://fa.wikipedia.org/wiki/مکانیک-کوانتومی')
     expect(URLfix("Sample Text")).toEqual("Sample Text");
     expect(URLfix()).toBeUndefined();
 });
