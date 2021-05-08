7f0068409a8bbb1a72e024bbf3d5dffd33e63337
Amirhossein Douzandeh Zenoozi <amirzenoozi72@gmail.com>
2021-05-08 15:29:10 +0430

    :art: Update Interface Name

--- src/modules/numberplate/types.skip.ts
+++ src/modules/numberplate/types.skip.ts
@@ -32,7 +32,7 @@ export enum PlateTypes {
 export type PlateResultApiTypeString = keyof typeof PlateTypes;
 
 
-export interface PlateResultApiTypeObject {
+export interface PlateResultApiTypeDetailsObject {
 	first_two_digits: string;
 	plate_character: string;
 	next_three_digits: string;
@@ -46,7 +46,7 @@ export interface PlateResultApi {
 	template: string;
 	province: string | null;
 	type: PlateResultApiTypeString;
-	typeDetails: PlateResultApiTypeObject;
+	typeDetails: PlateResultApiTypeDetailsObject;
 	category: string | null;
 }
 
