acf2ab3d384d7c06629f6e79c8c15ace9ede13a4
Saeed <hassanisaeed19@yahoo.com>
2021-06-08 14:49:57 +0430

    fix: check format of input is valid

--- test/timeAgo.spec.ts
+++ test/timeAgo.spec.ts
@@ -1,44 +1,53 @@
-import {digitsFaToEn, TimeAgo} from "../src";
+import { digitsFaToEn, timeAgo } from '../src';
+import { checkFormatDateTime } from '../src/modules/timeAgo';
 
-function getTime(second:number) {
-    const date: string[] = new Date(Date.now() + second).toLocaleDateString('fa-IR', {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-    }).split('/');
-    const time: string[] = new Date(Date.now() +second ).toLocaleTimeString('fa-IR', {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-    }).split(':');
+function getTime(second: number) {
+	const date: string[] = new Date(Date.now() + second)
+		.toLocaleDateString('fa-IR', {
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit',
+		})
+		.split('/');
+	const time: string[] = new Date(Date.now() + second)
+		.toLocaleTimeString('fa-IR', {
+			hour: '2-digit',
+			minute: '2-digit',
+			second: '2-digit',
+		})
+		.split(':');
 
-    // console.log(date)
-    // console.log(time)
-
-    return digitsFaToEn(date[0] + '-' + date[1] + '-' + date[2] + ' ' +
-        time[0] + ':' + time[1] + ':' + time[2])
+	return digitsFaToEn(date[0] + '-' + date[1] + '-' + date[2] + ' ' + time[0] + ':' + time[1] + ':' + time[2]);
 }
 
-describe("TimeAgo", () => {
-    it("Previous", () => {
-        expect(TimeAgo(getTime(-10*1000 ))).toEqual("10 ثانیه قبل");
-        expect(TimeAgo(getTime( -3*60*1000))).toEqual("3 دقیقه قبل");
-        expect(TimeAgo(getTime( -18*60*60*1000))).toEqual("18 ساعت قبل");
-        expect(TimeAgo(getTime( -7*24*60*60*1000))).toEqual("حدود 7 روز قبل");
-        expect(TimeAgo(getTime( -7*30*24*60*60*1000))).toEqual("حدود 7 ماه قبل");
-        expect(TimeAgo(getTime( -14*30*24*60*60*1000))).toEqual("حدود 1 سال قبل");
-    });
+describe('timeAgo', () => {
+	it('Previous', () => {
+		expect(timeAgo(getTime(-10 * 1000))).toEqual('10 ثانیه قبل');
+		expect(timeAgo(getTime(-3 * 60 * 1000))).toEqual('3 دقیقه قبل');
+		expect(timeAgo(getTime(-18 * 60 * 60 * 1000))).toEqual('18 ساعت قبل');
+		expect(timeAgo(getTime(-7 * 24 * 60 * 60 * 1000))).toEqual('حدود 7 روز قبل');
+		expect(timeAgo(getTime(-7 * 30 * 24 * 60 * 60 * 1000))).toEqual('حدود 7 ماه قبل');
+		expect(timeAgo(getTime(-14 * 30 * 24 * 60 * 60 * 1000))).toEqual('حدود 1 سال قبل');
+	});
 
-    it("Now", () => {
-        expect(TimeAgo(getTime( 0))).toEqual("اکنون");
-    });
+	it('Now', () => {
+		expect(timeAgo(getTime(0))).toEqual('اکنون');
+	});
 
-    it("Next", () => {
-        expect(TimeAgo(getTime(10*1000 ))).toEqual("10 ثانیه بعد");
-        expect(TimeAgo(getTime( 3*60*1000))).toEqual("3 دقیقه بعد");
-        expect(TimeAgo(getTime( 18*60*60*1000))).toEqual("18 ساعت بعد");
-        expect(TimeAgo(getTime( 7*24*60*60*1000))).toEqual("حدود 7 روز بعد");
-        expect(TimeAgo(getTime( 7*30*24*60*60*1000))).toEqual("حدود 7 ماه بعد");
-        expect(TimeAgo(getTime( 14*30*24*60*60*1000))).toEqual("حدود 1 سال بعد");
-    });
+	it('Next', () => {
+		expect(timeAgo(getTime(10 * 1000))).toEqual('10 ثانیه بعد');
+		expect(timeAgo(getTime(3 * 60 * 1000))).toEqual('3 دقیقه بعد');
+		expect(timeAgo(getTime(18 * 60 * 60 * 1000))).toEqual('18 ساعت بعد');
+		expect(timeAgo(getTime(7 * 24 * 60 * 60 * 1000))).toEqual('حدود 7 روز بعد');
+		expect(timeAgo(getTime(7 * 30 * 24 * 60 * 60 * 1000))).toEqual('حدود 7 ماه بعد');
+		expect(timeAgo(getTime(14 * 30 * 24 * 60 * 60 * 1000))).toEqual('حدود 1 سال بعد');
+	});
+
+	it('Check Input Regex', () => {
+		expect(checkFormatDateTime('1400-03-18 12:22:14')).toEqual(true);
+		expect(checkFormatDateTime('1400-03-18 12:2:4')).toEqual(true);
+		expect(checkFormatDateTime('1400-03-18 12:22')).toEqual(false);
+		expect(checkFormatDateTime('1400-03-18 12:2:455')).toEqual(false);
+		expect(checkFormatDateTime('1400/03/18 12:2:45')).toEqual(false);
+	});
 });
