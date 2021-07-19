5477db42a4b63d1ccc1d78aaf34fb391a249d6e2
torki <a.torki@hasintech.com>
2021-07-19 13:02:46 +0430

    test(timeAgo): add timeZone to toLocaleString

--- test/timeAgo.spec.ts
+++ test/timeAgo.spec.ts
@@ -10,6 +10,7 @@ function getTime(second: number) {
 			hour: "2-digit",
 			minute: "2-digit",
 			second: "2-digit",
+			timeZone: "Asia/Tehran",
 		})
 		.replace(/‏|،/g, "");
 	return digitsFaToEn(currentDateTime);
