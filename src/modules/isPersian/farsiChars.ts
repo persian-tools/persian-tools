9fccbe69fae927e25ea22305cfdae04ad48620bb
Mohammad Ghonchesefidi <ranger.m.gh@gmail.com>
2021-07-27 23:34:12 +0430

    Fix Typo

--- src/modules/isPersian/farsiChars.ts
+++ src/modules/isPersian/farsiChars.ts
@@ -1,7 +1,7 @@
 export const faAlphabet = "ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی";
 export const faNumber = "۰۱۲۳۴۵۶۷۸۹";
-export const faShortVowells = "َُِ";
+export const faShortVowels = "َُِ";
 export const faOthers = "‌آاً";
 export const faMixedWithArabic = "ًٌٍَُِّْٰٔءك‌ةۀأإيـئؤ،";
-export const faText = faAlphabet + faNumber + faShortVowells + faOthers;
+export const faText = faAlphabet + faNumber + faShortVowels + faOthers;
 export const faComplexText = faText + faMixedWithArabic;
