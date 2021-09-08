656d1b2ddeaa8232952df45ff26db52e51e2340e
PSPARSA <57572461+psparsa@users.noreply.github.com>
2021-09-08 21:04:20 +0430

    test(getBankNameFromCardNumber): correction of invalid test cases

--- test/getBankNameFromCardNumber.spec.ts
+++ test/getBankNameFromCardNumber.spec.ts
@@ -4,8 +4,6 @@ it("Get the name of the bank by bank account number", () => {
 	expect(getBankNameFromCardNumber(6037701689095443)).toEqual("بانک کشاورزی");
 	expect(getBankNameFromCardNumber(6219861034529007)).toEqual("بانک سامان");
 	expect(getBankNameFromCardNumber("6219861034529007")).toEqual("بانک سامان");
-
-	expect(getBankNameFromCardNumber("621986103452900")).toBeNull();
-	expect(getBankNameFromCardNumber("9999991034529007")).toBeNull();
-	expect(getBankNameFromCardNumber()).toBeUndefined();
+	expect(getBankNameFromCardNumber("621986103452900")).toBeUndefined();
+	expect(getBankNameFromCardNumber("9999991034529007")).toBeUndefined();
 });
