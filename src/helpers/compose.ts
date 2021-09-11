3da563831c8593cc4346fb09cb87565cb789de58
PSPARSA <57572461+psparsa@users.noreply.github.com>
2021-09-11 22:34:51 +0430

    feat(helpers): the compose module implemented

---
+++ src/helpers/compose.ts
@@ -0,0 +1,22 @@
+type Func<I, O> = (input: I) => O;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Compose = (...functions: Func<any, any>[]) => Func<any, any>;
+
+/**
+ * Performs right-to-left function composition. The arguments
+ * should be unary function
+ *
+ * **Note:** The result of compose is not automatically curried.
+ *
+ * @sig ((y -> z), (x -> y), ..., (o -> p), ((a, b, ..., n) -> o)) -> ((a, b, ..., n) -> z)
+ * @param {...Function} ...functions The functions to compose
+ * @symb compose(f, g, h)(x) = f(g(h(x)))
+ */
+
+const compose: Compose =
+	(...functions) =>
+	(arg) =>
+		functions.reduceRight((prev, current) => current(prev), arg);
+
+export { compose, Compose };
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,3 +2,4 @@ export * from "./trim";
 export * from "./replaceArray";
 export * from "./arabicContextualForms";
 export * from "./kindOf";
+export * from "./compose";
