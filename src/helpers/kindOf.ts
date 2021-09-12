bdce44636e109a64169672ba13c18b42869cb1ad
PSPARSA <57572461+psparsa@users.noreply.github.com>
2021-09-12 19:30:19 +0430

    chore(helpers/kindOf): removing the `KindOf` type

--- src/helpers/kindOf.ts
+++ src/helpers/kindOf.ts
@@ -1,3 +1,1 @@
-export type KindOf = (inp: unknown) => string;
-
-export const kindOf: KindOf = (inp) => Object.prototype.toString.call(inp).slice(8, -1).toLowerCase();
+export const kindOf = (inp: unknown): string => Object.prototype.toString.call(inp).slice(8, -1).toLowerCase();
