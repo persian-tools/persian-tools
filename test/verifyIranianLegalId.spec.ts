50c82591eaf4ea62379fb1eafd3713218d6eb16f
Mahdi <mahdimomeni6@gmail.com>
2021-12-05 23:23:59 +0330

    style: reformat tests

--- test/verifyIranianLegalId.spec.ts
+++ test/verifyIranianLegalId.spec.ts
@@ -7,8 +7,8 @@ it("Validation of Iranian Legal Number", () => {
 	expect(verifyIranianLegalId("1111111111")).toBeFalsy();
 	expect(verifyIranianLegalId(10380284792)).toBeFalsy();
 	expect(verifyIranianLegalId(10380285692)).toBeFalsy();
+	expect(verifyIranianLegalId("09748208301")).toBeFalsy();
 
 	expect(verifyIranianLegalId(10380284790)).toBeTruthy();
 	expect(verifyIranianLegalId("10380284790")).toBeTruthy();
-	expect(verifyIranianLegalId("09748208301")).toBeFalsy();
 });
