6e53ad82a737879dda9997314e688e545af56ecc
morteza <mo.jrn68@gmail.com>
2022-01-03 17:13:11 +0330

    fix(nationalId): replace depricated method

--- src/modules/nationalId/index.ts
+++ src/modules/nationalId/index.ts
@@ -24,14 +24,14 @@ function verifyIranianNationalId(nationalId?: string | number): boolean | undefi
 
 	if (code in notAllowedDigits) return false;
 	if (codeLength < 8 || codeLength > 10) return false;
-	code = ("00" + code).substr(codeLength + 2 - 10);
-	if (+code.substr(3, 6) === 0) return false;
+	code = ("00" + code).substring(codeLength + 2 - 10);
+	if (+code.substring(3, 9) === 0) return false;
 
-	const lastNumber = +code.substr(9, 1);
+	const lastNumber = +code.substring(9);
 	let sum = 0;
 
 	for (let i = 0; i < 9; i++) {
-		sum += +code.substr(i, 1) * (10 - i);
+		sum += +code.substring(i, i + 1) * (10 - i);
 	}
 
 	sum = sum % 11;
