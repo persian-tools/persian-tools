38fdc57e87e863a01dbde01bdaf1e32426473c6e
mediv0 <anonymousaxis@gmail.com>
2022-05-03 20:39:57 +0430

    feat: phoneNumberNormalizer

--- src/modules/phoneNumber/index.ts
+++ src/modules/phoneNumber/index.ts
@@ -1,3 +1,4 @@
 export { default as phoneNumberDetail } from "./detail";
 export { default as phoneNumberValidator } from "./validator";
+export { default as phoneNumberNormalizer } from "./normalizer";
 export { OperatorModel } from "./utils";
