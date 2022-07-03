a3fddd370ee27b0835a3210f563a4a7a637ae19f
amirali <amiraliyavari785@gmail.com>
2022-07-03 13:36:52 -0700

    feat: make test file

---
+++ test/findCapitalByStateName.spec.ts
@@ -0,0 +1,12 @@
+import { findCapitalByStateName } from '../src/modules/findCapitalByStateName/'
+
+it("findCapitalByStateName", () => {
+	expect(findCapitalByStateName("کهگیلویه و بویراحمد")).toBeTruthy();
+	expect(findCapitalByStateName("خراسان رضوی")).toBeTruthy();
+
+	expect(findCapitalByStateName("Lorem")).toBeFalsy();
+	expect(findCapitalByStateName("خراسان")).toBeFalsy();
+	expect(findCapitalByStateName("استانی که وجود ندارد")).toBeFalsy();
+	expect(findCapitalByStateName("State")).toBeFalsy();
+	expect(findCapitalByStateName("")).toBeFalsy();
+});
