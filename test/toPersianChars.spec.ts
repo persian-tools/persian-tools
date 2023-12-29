4e2c6c8392b77016eb4d89aaf5b82d456fd99a34
ali-master <ali_4286@live.com>
2023-12-29 22:35:02 +0330

    fix(toPersianChars): wrong result in converting ی | reported by #344

--- test/toPersianChars.spec.ts
+++ test/toPersianChars.spec.ts
@@ -3,5 +3,6 @@ import { toPersianChars } from "../src";
 
 it("toPersianChars", () => {
 	expect(toPersianChars("علي")).toEqual("علی");
+	expect(toPersianChars("تلفن همراه")).toEqual("تلفن همراه");
 	expect(toPersianChars("")).toBeFalsy();
 });
