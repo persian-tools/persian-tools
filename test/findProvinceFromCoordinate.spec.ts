e05d9c97593e575562e793d3cc2889e468763fec
Moein Moeinnia <69215813+Moeinmn@users.noreply.github.com>
2024-08-12 19:48:37 +0330

    feat: find province form coordinate module (#361)
    Co-authored-by: moein moeinnia <moein@pro.ai>

---
+++ test/findProvinceFromCoordinate.spec.ts
@@ -0,0 +1,32 @@
+import { findProvinceFromCoordinate } from "../src";
+import { it, expect } from "vitest";
+
+it("should throw an error when there isn't any coordinate", () => {
+	expect(() => {
+		findProvinceFromCoordinate({
+            longitude: 1,
+            latitude: 2
+        });
+	}).toThrow();
+});
+
+it("Should return the correct province for a given coordinate", () => {
+
+    const pointToCheck = { longitude: 51.38897, latitude: 35.6892 };
+
+    const {fa , en } = findProvinceFromCoordinate(pointToCheck);
+  
+    expect(fa).toBe('تهران');
+    expect(en).toBe('Tehran');
+});
+
+it("Should return the correct province for a coordinate in Isfahan", () => {
+
+    const pointToCheck = { longitude: 51.6660, latitude: 32.6546 };
+  
+    const {fa , en } = findProvinceFromCoordinate(pointToCheck);
+  
+    expect(fa).toBe('اصفهان');
+    expect(en).toBe('Esfahan');
+  });
+
