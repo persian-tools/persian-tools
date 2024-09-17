c375a4d09cd76afe86cf5258f5cebee69cd0ec93
Mostafa Ahangarha <ahangarha@riseup.net>
2024-09-17 21:58:00 +0330

    feat: improve halfSpace func detection
    This commit, improves the detection when there is no space after ها and
    تر.

--- test/halfSpace.spec.ts
+++ test/halfSpace.spec.ts
@@ -10,6 +10,33 @@ describe("halfSpace", () => {
 		expect(halfSpace("")).toBeFalsy();
 	});
 
+	it("gets applied on ending ها and تر", () => {
+		const pairs = [
+			{actual: "درخت ها", expected: "درخت‌ها"},
+			{actual: "درخت های", expected: "درخت‌های"},
+			{actual: "درخت هایش", expected: "درخت‌هایش"},
+			{actual: "بزرگ تر", expected: "بزرگ‌تر"},
+		]
+
+		pairs.forEach(({actual, expected}) => {
+			expect(halfSpace(actual)).toEqual( expected)
+		})
+	})
+
+	it("gets applied on ها and تر followed by special characters", () => {
+		const pairs = [
+			{actual: "درخت ها، باد", expected: "درخت‌ها، باد"},
+			{actual: "«درخت های»", expected: "«درخت‌های»"},
+			{actual: "(درخت هایش)", expected: "(درخت‌هایش)"},
+			{actual: "بزرگ تر،", expected: "بزرگ‌تر،"},
+
+		]
+
+		pairs.forEach(({actual, expected}) => {
+			expect(halfSpace(actual)).toEqual( expected)
+		})
+	})
+
 	it("should throw", () => {
 		expect(() => {
 			// @ts-ignore
