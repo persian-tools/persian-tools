46a61d7558ef938a568ee8b6dd1d67daaa0882d9
Mohammad Rostami <mohammad.rostami13@gmail.com>
2024-09-24 16:37:19 +0330

    fixed : rename variables and adding functions for easy understanding and debugging

--- src/modules/nationalId/index.ts
+++ src/modules/nationalId/index.ts
@@ -7,7 +7,7 @@
  */
 
 function allCharactersSame(inputString?: string): boolean {
-	if (!inputString){
+	if (!inputString) {
 		return false;
 	}
 
@@ -19,6 +19,21 @@ function allCharactersSame(inputString?: string): boolean {
 	return true;
 }
 
+function calculateCarry(nationalCode: string): number {
+	let carry = 0;
+	for (let i = 0; i < 9; i++) carry += parseInt(nationalCode?.substr(i, 1), 10) * (10 - i);
+	carry %= 11;
+	return carry
+}
+
+function checkVerification(carry: number, lastDigit: number): boolean {
+	if (carry < 2 && lastDigit === carry) {
+		return true
+	} else if (carry >= 2 && lastDigit === 11 - carry) {
+		return true
+	} else return false
+}
+
 
 /**
  * Check National-id validation
@@ -28,23 +43,25 @@ function allCharactersSame(inputString?: string): boolean {
  * @return {Boolean}                    [valid or no]
  */
 
-function verifyIranianNationalId(nationalCode?: string | number): boolean | undefined {
-	if (!nationalCode){
+function verifyIranianNationalId(nationalCode: string | number): boolean {
+	if (!nationalCode) {
 		return false
 	}
 
-	let nationalCodeInString  = nationalCode.toString();
+	let nationalCodeInString = nationalCode?.toString();
 
-	if (!nationalCode || nationalCode?.toString().length !== 10 || allCharactersSame(nationalCodeInString )) {
+	if (!nationalCode || nationalCode?.toString().length !== 10 || allCharactersSame(nationalCodeInString)) {
 		return false;
 	}
-	nationalCodeInString  = `0000${nationalCodeInString }`.substr(nationalCodeInString ?.length + 4 - 10);
-	if (parseInt(nationalCodeInString .substr(3, 6), 10) === 0) return false;
-	const c = parseInt(nationalCodeInString .substr(9, 1), 10);
-	let s = 0;
-	for (let i = 0; i < 9; i++) s += parseInt(nationalCodeInString .substr(i, 1), 10) * (10 - i);
-	s %= 11;
-	return (s < 2 && c === s) || (s >= 2 && c === 11 - s)
+
+	nationalCodeInString = `0000${nationalCodeInString}`.substr(nationalCodeInString?.length + 4 - 10);
+	if (parseInt(nationalCodeInString.substr(3, 6), 10) === 0) return false;
+
+	const nationalCodeLastDigit = parseInt(nationalCodeInString.substr(9, 1), 10);
+
+	const carryValue = calculateCarry(nationalCodeInString)
+
+	return checkVerification(carryValue, nationalCodeLastDigit)
 }
 
 export default verifyIranianNationalId;
